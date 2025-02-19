27089501f8cd53f8ef8ced7cec2e4ad114e9ffea
K900 <me@0upti.me>
2025-02-19 19:38:19 +0300

    mesa-radeonsi-jupiter: track latest patches as of 24.3.4.197725.radeonsi_24.3.4-1

--- pkgs/mesa-radeonsi-jupiter/default.nix
+++ pkgs/mesa-radeonsi-jupiter/default.nix
@@ -4,13 +4,8 @@ mesa.overrideAttrs(old: {
   patches = old.patches ++ [
     # Cherry-pick the swapchain override bits from the Valve 24.3 branch
     (fetchpatch {
-      url = "https://github.com/Jovian-Experiments/mesa/commit/27482ce2030cc4249908382fb4e2092134135fdb.patch";
-      hash = "sha256-tQoGN/88sdPDQmscfkPKulPasU2tLeRekWG9xWF/BsQ=";
-    })
-    # Disable glthread by default
-    (fetchpatch {
-      url = "https://github.com/Jovian-Experiments/mesa/commit/09d9c2fd7f69fbe59aab0bb53438d3446ea90dd4.patch";
-      hash = "sha256-y0wGWVBQiWd2hOqiqDIKP9VERNSP4owuPGjnqUTNy68=";
+      url = "https://github.com/Jovian-Experiments/mesa/commit/8d0948917e651b6265bb35eab340d06eb8988c44.patch";
+      hash = "sha256-PQbMfKMGabq0n4/JRhfxHl7TV88Ddf9QolqNIgdXOOk=";
     })
   ];
 })
