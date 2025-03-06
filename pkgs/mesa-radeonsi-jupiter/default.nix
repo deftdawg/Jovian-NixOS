2975f18876bfef16419455cc5dac1eaf9645d443
K900 <me@0upti.me>
2025-03-06 11:58:16 +0300

    mesa-radeonsi-jupiter: refresh frame limiter patch

--- pkgs/mesa-radeonsi-jupiter/default.nix
+++ pkgs/mesa-radeonsi-jupiter/default.nix
@@ -4,8 +4,8 @@ mesa.overrideAttrs(old: {
   patches = old.patches ++ [
     # Cherry-pick the swapchain override bits from the Valve 24.3 branch
     (fetchpatch {
-      url = "https://github.com/Jovian-Experiments/mesa/commit/8d0948917e651b6265bb35eab340d06eb8988c44.patch";
-      hash = "sha256-PQbMfKMGabq0n4/JRhfxHl7TV88Ddf9QolqNIgdXOOk=";
+      url = "https://github.com/Jovian-Experiments/mesa/commit/27482ce2030cc4249908382fb4e2092134135fdb.patch";
+      hash = "sha256-tQoGN/88sdPDQmscfkPKulPasU2tLeRekWG9xWF/BsQ=";
     })
   ];
 })
