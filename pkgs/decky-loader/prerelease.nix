6ed8ac628823671d345742f574956f7c67e92a1c
K900 <me@0upti.me>
2025-07-14 22:57:24 +0300

    decky-loader-prerelease: 3.1.8-pre1 -> 3.1.9-pre1, adjust for nixpkgs changes

--- pkgs/decky-loader/prerelease.nix
+++ pkgs/decky-loader/prerelease.nix
@@ -5,18 +5,19 @@
 }:
 decky-loader.overridePythonAttrs rec {
   pname = "decky-loader";
-  version = "3.1.8-pre1";
+  version = "3.1.9-pre1";
 
   src = fetchFromGitHub {
     owner = "SteamDeckHomebrew";
     repo = "decky-loader";
     rev = "v${version}";
-    hash = "sha256-eaKtmb7qtQLH8r5Y8ZO0Kc5K2IT97Zv4F4DGtblO6jA=";
+    hash = "sha256-EzUgZ1IdzS6H3eknfnTiqBppqZa+LvwaBikjTLs6sVs=";
   };
 
   pnpmDeps = pnpm_9.fetchDeps {
+    fetcherVersion = 1;
     inherit pname version src;
     sourceRoot = "${src.name}/frontend";
-    hash = "sha256-Gg7e1y+gDIT5du6u8HdxF9pNAPjkQSK4Q+K7xpxm+Ps=";
+    hash = "sha256-HS3PWLxIoH/1/ir510eOLEMVMEdXBOhvYHTZBMnCB2Q=";
   };
 }
